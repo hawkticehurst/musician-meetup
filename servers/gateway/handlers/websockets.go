95ca946e4c74d666a7b50ab448fb5c6d93863cdc
piercecave <piercecave@yahoo.com>
2019-12-03 00:25:33 -0800

    edited gateway

--- servers/gateway/handlers/websockets.go
+++ servers/gateway/handlers/websockets.go
@@ -2,36 +2,35 @@ package handlers
 
 import (
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		// This function's purpose is to reject websocket upgrade requests if the
-		// origin of the websockete handshake request is coming from unknown domains.
-		// This prevents some random domain from opening up a socket with your server.
-		// TODO: make sure you modify this for your HW to check if r.Origin is your host
-		return true
-	},
-}
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// 	CheckOrigin: func(r *http.Request) bool {
+// 		// This function's purpose is to reject websocket upgrade requests if the
+// 		// origin of the websockete handshake request is coming from unknown domains.
+// 		// This prevents some random domain from opening up a socket with your server.
+// 		// TODO: make sure you modify this for your HW to check if r.Origin is your host
+// 		return true
+// 	},
+// }
 
 // WebSocketConnectionHandler upgrades a client connection to a WebSocket connection,
 // regardless of what method is used in the request
-func WebSocketConnectionHandler(w http.ResponseWriter, r *http.Request) {
+func (hc *Context) WebSocketConnectionHandler(w http.ResponseWriter, r *http.Request) {
 	// handle the websocket handshake
-	if r.Header.Get("Origin") != "https://client.info441summary.me" {
-		http.Error(w, "Websocket Connection Refused", 403)
-	} else {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			http.Error(w, "Failed to open websocket connection", 401)
-		}
-		// do something with connection
-		conn.WriteMessage(1, []byte("Hello jackass\n"))
-	}
+	// if r.Header.Get("Origin") != "https://client.info441summary.me" {
+	// 	http.Error(w, "Websocket Connection Refused", 403)
+	// 	return
+	// }
+	// conn, err := upgrader.Upgrade(w, r, nil)
+	// if err != nil {
+	// 	http.Error(w, "Failed to open websocket connection", 401)
+	// }
+	// // do something with connection
+	// conn.WriteMessage(1, []byte("Hello jackass\n"))
+	w.Write([]byte("Made connection"))
 }
 
 // package handlers
