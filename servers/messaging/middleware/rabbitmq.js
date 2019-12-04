2886f6887d9083f859ed5932a0d88a72248d7779
Hawk Ticehurst <hawkticehurst@gmail.com>
2019-12-03 21:06:15 -0800

    I do not know what I changed but I changed something

--- servers/messaging/middleware/rabbitmq.js
+++ servers/messaging/middleware/rabbitmq.js
@@ -9,16 +9,16 @@ async function getRabbitMQConnection(req, res, next) {
         var amqpConn = null;
         var amqpChannel = null;
         // Create connection with RabbitMQ
-        amqp.connect('amqp://rabbitmqserver:5672', function(error0, connection) {
+        amqp.connect('amqp://rabbitmqserver:5672', function (error0, connection) {
             if (error0) {
                 throw error0;
             }
-            connection.createChannel(function(error1, channel) {
+            connection.createChannel(function (error1, channel) {
                 if (error1) {
                     throw error1;
                 }
                 var queue = 'events';
-            
+
                 channel.assertQueue(queue, {
                     durable: true
                 });
@@ -27,15 +27,15 @@ async function getRabbitMQConnection(req, res, next) {
             });
             amqpConn = connection;
         });
-      // Store reference to rabbitmq connection and channel
-      req.amqpConn = amqpConn;
-      req.amqpChannel = amqpChannel;
-      next();
-    } catch(err) {
-      console.error(err);
-      res.set("Content-Type", "text/plain");
-      res.status(404).send("RabbitMQ Error: Cannot connect to rabbitMQ server.");
-      return
+        // Store reference to rabbitmq connection and channel
+        req.amqpConn = amqpConn;
+        req.amqpChannel = amqpChannel;
+        next();
+    } catch (err) {
+        console.error(err);
+        res.set("Content-Type", "text/plain");
+        res.status(404).send("RabbitMQ Error: Cannot connect to rabbitMQ server.");
+        return
     }
 }
 
